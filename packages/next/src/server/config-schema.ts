b17fbcc02aea3aa4e1e3e48a74e72fc018629968
Ben Gubler <nebrelbug@gmail.com>
2025-07-03 16:03:20 -0700

    feat: generate route types manifest

--- packages/next/src/server/config-schema.ts
+++ packages/next/src/server/config-schema.ts
@@ -449,6 +449,7 @@ export const configSchema: zod.ZodType<NextConfig> = z.lazy(() =>
           ])
           .optional(),
         typedRoutes: z.boolean().optional(),
+        newTypedRoutes: z.boolean().optional(),
         webpackBuildWorker: z.boolean().optional(),
         webpackMemoryOptimizations: z.boolean().optional(),
         /**
